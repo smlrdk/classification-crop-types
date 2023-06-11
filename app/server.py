953979e939c7aa0e044a2af8d6090de38e2cc4e3
Maria Solodkaya <solodkaya2013masha@gmail.com>
2023-06-12 00:32:43 +0300

    update server

--- app/server.py
+++ app/server.py
@@ -83,5 +83,7 @@ def load_table():
 #     app.run(debug=True, host='192.168.1.68', port=port)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='127.0.0.1', port=port)
+    # port = int(os.environ.get('PORT', 5000))
+    # app.run(debug=True, host='127.0.0.1', port=port)
+
+    app.run(debug=True, host='0.0.0.0')
--- app/start_docker.py
+++ app/start_docker.py
@@ -1,4 +1,4 @@
 import os 
 
-command = f"docker run -p 5000:5000 classification-crop-types"
+command = f"docker run -p 5000:5000 crop-types"
 os.system(command)
